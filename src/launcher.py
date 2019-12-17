38c780daceb3656fb7b28fc57e4b0e49f32fa317
Crisxz1891 <crisxz1891@gmail.com>
2019-12-17 06:27:19 -0300

    Add files via upload

---
+++ src/launcher.py
@@ -0,0 +1,20 @@
+import pygame
+import game
+import menu
+import puntajes
+
+def main():
+    pygame.init()
+    size = [game.constants.SCREEN_WIDTH, game.constants.SCREEN_HEIGHT]
+    screen = pygame.display.set_mode(size)   
+    pygame.display.set_caption("Shinobi Road")
+    opc = "Menu"
+    while opc != "Salir":
+        if opc=="Menu":
+            opc = menu.menu.main(screen)
+        elif opc=="Jugar":
+            opc = game.ShinobiRoad.main(screen)
+        elif opc=="Puntajes":
+            opc = puntajes.puntajes.main(screen)
+    pygame.quit()
+main()
